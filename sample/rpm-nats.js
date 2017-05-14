72ff560089b05e3da29aecfc792a7d75081b9102
Ralf S. Engelschall <rse@engelschall.com>
2017-05-14 11:20:11 +0200

    add NATS testing script

---
+++ sample/rpm-nats.js
@@ -0,0 +1,18 @@
+
+/* eslint no-console: off */
+
+const PubSub = require("..")
+
+;(async () => {
+    let pubsub = new PubSub("rpm+nats://example:example@127.0.0.1:4242/example")
+    await pubsub.open()
+    await pubsub.subscribe("foo/#", (value, channel) => {
+        console.log("GOT", value, channel)
+    })
+    await pubsub.publish("foo/bar", "bar")
+    await pubsub.publish("foo/baz", "baz")
+    setTimeout(async () => {
+        await pubsub.close()
+    }, 1000)
+})()
+

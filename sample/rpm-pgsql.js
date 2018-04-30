2b487de84718bc6063f02bbcb8f861ee8c0f74f1
Ralf S. Engelschall <rse@engelschall.com>
2018-04-30 21:53:29 +0200

    add PostgreSQL sample

---
+++ sample/rpm-pgsql.js
@@ -0,0 +1,16 @@
+
+/* eslint no-console: off */
+
+const PubSub = require("..")
+
+;(async () => {
+    let pubsub = new PubSub("rpm+pgsql://postgresql:postgresql@127.0.0.1:5432/template1")
+    await pubsub.open()
+    await pubsub.subscribe("foo-bar.quux", (value, channel) => {
+        console.log("GOT", value, channel)
+    })
+    await pubsub.publish("foo-bar.quux", "bar")
+    await pubsub.publish("foo-bar.quux", "baz")
+    await pubsub.close()
+})()
+
